1852cb056e5fe81488eafe5ea8113e6fec414d84
Lawrence ime <lawrenceime246@gmail.com>
2023-09-13 04:52:49 +0100

    Transaction component created

---
+++ src/components/TransactionPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransactionPage = () => {
+  return (
+    <div>
+        <h1> Transactions </h1>
+        
+      
+    </div>
+  )
+}
+
+export default TransactionPage
+
